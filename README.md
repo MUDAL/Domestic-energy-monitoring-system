26b7ad2472a75e79b2364a473c9d852dd324dba8
Olaoluwa Raji <olaoluwaraji1999@gmail.com>
2022-11-23 23:02:46 +0100

    initialCode

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Monitors parameters of a power transformer, logs the data, and uploads to Google
 1. https://randomnerdtutorials.com/esp32-esp8266-publish-sensor-readings-to-google-sheets/  
 2. https://github.com/stechiez/iot_projects/blob/master/GoogleSpreadSheet_ESP32_IFTTT/ESP32_GoogleSpreadSheetIFTTT/ESP32_GoogleSpreadSheetIFTTT.ino  
 
+## Precaution(s)  
+1. ``preferences.begin("T-Mon",false);``. I observed that a short string as the first argument    
+of ``begin`` method helps in avoiding some issues with accessing the flash memory via the   
+``Preferences`` library. ``T-Mon`` was used as a short form for ``Transformer-monitor``.  
+
+ 
---
+++ Transformer_monitor/Transformer_monitor.ino
@@ -0,0 +1,174 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Preferences.h>
+#include <WiFiManager.h>
+
+//RTOS
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE  0
+#else
+#define ARDUINO_RUNNING_CORE  1
+#endif
+
+//Defines
+//Maximum number of characters for IFTTT details
+#define SIZE_EVENT_NAME        20
+#define SIZE_IFTTT_KEY         50
+
+//Shared resources
+//Define textboxes for IFTTT event name and key  
+WiFiManagerParameter eventName("event","Event name","",SIZE_EVENT_NAME);
+WiFiManagerParameter iftttKey("key","IFTTT key","",SIZE_IFTTT_KEY);
+Preferences preferences; //for accessing ESP32 flash memory
+
+//String eventName = "Power data";
+//String key = "c8wyfZGxnyQjACiOYVsSI6";
+
+/**
+ * @brief Make an HTTP GET request to the specified server
+*/
+static void HttpGetRequest(const char* serverName) 
+{ 
+  HTTPClient http;
+  http.begin(serverName);
+  int httpResponseCode = http.GET();
+  if(httpResponseCode == HTTP_CODE_OK) 
+  {
+    Serial.println("Successful request ");
+  } 
+  else 
+  {
+    Serial.print("HTTP error code: ");
+    Serial.println(httpResponseCode);
+  }
+  http.end();  
+}
+
+/**
+ * @brief Store new data (IFTTT credentials) to specified location  
+ * in ESP32's flash memory if the new credentials are different from the previous.  
+*/
+static void StoreNewFlashData(const char* flashLoc,const char* newData,
+                              const char* oldData,uint8_t dataSize)
+{
+  if(strcmp(newData,"") && strcmp(newData,oldData))
+  {
+    preferences.putBytes(flashLoc,newData,dataSize);
+  }
+}
+
+void setup() 
+{
+  setCpuFrequencyMhz(80);
+  Serial.begin(115200);
+  preferences.begin("T-Mon",false); //T-Mon : Transformer monitor
+  //Create tasks
+  xTaskCreatePinnedToCore(WiFiManagementTask,"",7000,NULL,1,NULL,ARDUINO_RUNNING_CORE);
+  xTaskCreatePinnedToCore(GoogleSheetTask,"",8000,NULL,1,NULL,ARDUINO_RUNNING_CORE);  
+}
+
+void loop() 
+{
+}
+
+/*
+ * @brief Manages WiFi configurations (STA and AP modes). Connects
+ * to an existing/saved network if available, otherwise it acts as
+ * an AP in order to receive new network credentials.
+*/
+void WiFiManagementTask(void* pvParameters)
+{
+  const uint16_t accessPointTimeout = 50000; //millisecs
+  static WiFiManager wm;
+  WiFi.mode(WIFI_STA);  
+  wm.addParameter(&eventName);
+  wm.addParameter(&iftttKey);
+  wm.setConfigPortalBlocking(false);
+  wm.setSaveParamsCallback(WiFiManagerCallback);   
+  //Auto-connect to previous network if available.
+  //If connection fails, ESP32 goes from being a station to being an access point.
+  Serial.print(wm.autoConnect("TRANSFORMER")); 
+  Serial.println("-->WiFi status");   
+  bool accessPointMode = false;
+  uint32_t startTime = 0;    
+  
+  while(1)
+  {
+    wm.process();
+    if(WiFi.status() != WL_CONNECTED)
+    {
+      if(!accessPointMode)
+      {
+        if(!wm.getConfigPortalActive())
+        {
+          wm.autoConnect("TRANSFORMER"); 
+        }
+        accessPointMode = true; 
+        startTime = millis(); 
+      }
+      else
+      {
+        //reset after a timeframe (device shouldn't spend too long as an access point)
+        if((millis() - startTime) >= accessPointTimeout)
+        {
+          Serial.println("\nAP timeout reached, system will restart for better connection");
+          vTaskDelay(pdMS_TO_TICKS(1000));
+          esp_restart();
+        }
+      }
+    }
+    else
+    {
+      if(accessPointMode)
+      {   
+        accessPointMode = false;
+        Serial.println("Successfully connected, system will restart now");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        esp_restart();
+      }
+    }    
+  }
+}
+
+/**
+ * @brief Uploads PZEM-004T module's data to Google sheets via 
+ * HTTP requests. IFTTT serves as an intermediary between  
+ * the ESP32 and Google sheets.  
+*/
+void GoogleSheetTask(void* pvParameters)
+{
+  char prevEventName[SIZE_EVENT_NAME] = {0};
+  char prevKey[SIZE_IFTTT_KEY] = {0};
+  uint32_t prevTime = millis();
+  while(1)
+  {
+    if(WiFi.status() == WL_CONNECTED && ((millis() - prevTime) >= 10000))
+    {
+      preferences.getBytes("event",prevEventName,SIZE_EVENT_NAME);
+      preferences.getBytes("key",prevKey,SIZE_IFTTT_KEY);  
+            
+      String iftttServerPath = "http://maker.ifttt.com/trigger/" + String(prevEventName) + 
+                         "/with/key/" + String(prevKey) + "?value1=" + String(112) 
+                         + "&value2="+String(225) +"&value3=" + String(777); 
+      HttpGetRequest(iftttServerPath.c_str());
+      prevTime = millis(); 
+    }
+  }
+}
+
+/*
+ * @brief Callback function that is called whenever WiFi
+ * manager parameters are received (in this case, IFTTT
+ * details)
+*/
+void WiFiManagerCallback(void) 
+{
+  char prevEventName[SIZE_EVENT_NAME] = {0};
+  char prevKey[SIZE_IFTTT_KEY] = {0};
+  preferences.getBytes("event",prevEventName,SIZE_EVENT_NAME);
+  preferences.getBytes("key",prevKey,SIZE_IFTTT_KEY);  
+  //Store IFTTT credentials in flash if the new ones are not the same as the old.  
+  StoreNewFlashData("event",eventName.getValue(),prevEventName,SIZE_EVENT_NAME);
+  StoreNewFlashData("key",iftttKey.getValue(),prevKey,SIZE_IFTTT_KEY);
+}
+
